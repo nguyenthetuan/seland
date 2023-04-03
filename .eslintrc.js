675f2e2098c53cdd0ca0a02eb643a742b7b349a4
trung.nguyen <trung.nguyen@vnextglobal.com>
2023-04-03 16:45:00 +0700

    Added .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,13 +25,6 @@ module.exports = {
     'react-native/no-inline-styles': 2,
     'react-native/no-raw-text': 2,
     'react-native/no-unused-styles': 2,
-    'react-native/sort-styles': [
-      2,
-      'asc',
-      {
-        ignoreClassNames: false,
-        ignoreStyleProperties: false,
-      },
-    ],
+    'react-native/sort-styles': 2,
   },
 };
