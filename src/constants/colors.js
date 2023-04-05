d8c3036ffb43dc32aa85886c4add98d0d05243af
trung.nguyen <trung.nguyen@vnextglobal.com>
2023-04-05 14:12:37 +0700

    Added colors

---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+export const COLOR_BLACK_1 = 'rgba(0,0,0, 0.85)';
+export const COLOR_BLACK_2 = 'rgba(0,0,0, 0.45)';
+export const COLOR_BLUE_1 = '#1d4279';
+export const COLOR_BLUE_2 = '#096dd9';
+export const COLOR_BLUE_3 = '#40a9ff';
+export const COLOR_GRAY = '#d9d9d9';
+export const COLOR_RED = '#f5222d';
+export const COLOR_WHITE = '#fff';
