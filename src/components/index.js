fea3780cc33565b903d1e86d5efcc44348162721
trung.nguyen <trung.nguyen@vnextglobal.com>
2023-04-06 14:48:37 +0700

    Added custom text

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Heading } from './Heading';
 export { default as Input } from './Input';
 export { default as Screen } from './Screen';
+export { default as Text } from './Text';
