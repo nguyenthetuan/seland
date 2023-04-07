4b17a27a374667ef60bbab614d069d17f0e72171
trung.nguyen <trung.nguyen@vnextglobal.com>
2023-04-07 16:57:30 +0700

    Added custom check box

---
+++ src/components/CheckBox/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  checkbox: {
+    padding: 0,
+  },
+});
+
+export default styles;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button';
+export { default as CheckBox } from './CheckBox';
 export { default as Heading } from './Heading';
 export { default as Input } from './Input';
 export { default as Screen } from './Screen';
