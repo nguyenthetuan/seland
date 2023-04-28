3702202f227fa716f56fafc2ed257003f5890458
trung.nguyen <trung.nguyen@vnextglobal.com>
2023-04-28 15:50:45 +0700

    Added custom select styles

---
+++ src/components/common/Select/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+import {
+  COLOR_BLACK_1,
+  COLOR_GRAY_5,
+  COLOR_WHITE,
+} from '../../../constants/colors';
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: COLOR_WHITE,
+    borderColor: COLOR_GRAY_5,
+    borderRadius: 8,
+    borderWidth: 1,
+    flex: 1,
+    width: '100%',
+  },
+  label: {
+    marginBottom: 8,
+  },
+  row: {
+    height: 40,
+  },
+  text: {
+    color: COLOR_BLACK_1,
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: 'left',
+  },
+});
+
+export default styles;
