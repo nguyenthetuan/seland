2f1640ff5b47f2f2dea323fc996f59a310a56d17
long.hoang <long.hoang@vnextglobal.com>
2023-05-13 23:21:02 +0700

    menu drawer

---
+++ src/screens/home/components/DrawerMenu/index.js
@@ -0,0 +1,96 @@
+import { Icon } from '@rneui/base';
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { Modal, TouchableOpacity, View } from 'react-native';
+
+import { Text } from '../../../../components';
+import { COLOR_WHITE } from '../../../../constants';
+import styles from './styles';
+
+const DrawerMenuHome = forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false);
+
+  const openDrawerMenu = () => setVisible(true);
+
+  const closeDrawerMenu = () => setVisible(false);
+
+  const onHandleMenu = () => {
+    closeDrawerMenu();
+  };
+
+  useImperativeHandle(ref, () => ({ openDrawerMenu }));
+  const menuList = [
+    {
+      name: 'Mua bán nhà đất',
+      key: 1,
+      onPress: onHandleMenu,
+    },
+    {
+      name: 'Cho thuê nhà đất',
+      key: 2,
+    },
+    {
+      name: 'Dự án',
+      key: 3,
+    },
+    {
+      name: 'Tin BĐS',
+      key: 4,
+    },
+    {
+      name: 'BĐS độc quyền',
+      key: 5,
+    },
+    {
+      name: 'Tra quy hoạch',
+      key: 6,
+    },
+    {
+      name: 'Tin tức',
+      key: 7,
+    },
+    {
+      name: 'Đào tạo',
+      key: 8,
+    },
+    {
+      name: 'Trở thành đại lý',
+      key: 9,
+    },
+    {
+      name: 'Quy hoạch',
+      key: 10,
+    },
+  ];
+
+  return (
+    <Modal
+      visible={visible}
+      transparent
+    >
+      <View style={styles.container}>
+        <View style={styles.drawer}>
+          {menuList.map(item => (
+            <TouchableOpacity
+              key={item?.key}
+              style={styles.btnMenu}
+              onPress={item?.onPress && item?.onPress}
+            >
+              <Text style={styles.textMenu}>{item?.name}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <View style={styles.boxClose}>
+          <Icon
+            color={COLOR_WHITE}
+            name="close"
+            onPress={closeDrawerMenu}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+});
+
+DrawerMenuHome.displayName = 'DrawerMenuHome';
+
+export default DrawerMenuHome;
