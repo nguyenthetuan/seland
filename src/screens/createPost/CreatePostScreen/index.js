1b2b011ec20364dc3a499eaf881dfce757b34dac
long.hoang <long.hoang@vnextglobal.com>
2023-05-17 18:13:52 +0700

    UI Post screen

--- src/screens/createPost/CreatePostScreen/index.js
+++ src/screens/createPost/CreatePostScreen/index.js
@@ -1,13 +1,100 @@
-import { Text } from '@rneui/themed';
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { Button, Icon } from '@rneui/base';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { SafeAreaView, ScrollView, View } from 'react-native';
 
+import { Save } from '../../../assets';
+import { Text } from '../../../components';
+import { COLOR_BLUE_1 } from '../../../constants';
+import BasicInformation from '../components/BasicInformation';
+import RealEstateInformation from '../components/RealEstateInformation';
 import styles from './styles';
 
-const CreatePostScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <Text>Create Post</Text>
-  </SafeAreaView>
-);
+const CreatePostScreen = () => {
+  const { t } = useTranslation();
+
+  const [tab, setTab] = useState(0);
+
+  const handleTab = value => {
+    setTab(value);
+  };
+
+  const renderTab = () => {
+    switch (tab) {
+      case 0:
+        return <BasicInformation />;
+      case 1:
+        return <RealEstateInformation />;
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <View style={styles.header}>
+          <Text style={styles.createPostNews}>
+            {t('common.createPostNews')}
+          </Text>
+          <Save />
+        </View>
+        <Text style={styles.youWant}>{t('common.youWant')}</Text>
+        <View style={styles.boxCheck}>
+          <Button
+            buttonStyle={styles.btnYouWant}
+            icon="save"
+            type="outline"
+          >
+            <Icon
+              color={COLOR_BLUE_1}
+              name="radio-button-unchecked"
+              size={20}
+            />
+            <Text style={styles.txtCheck}>{t('button.savePrivate')}</Text>
+          </Button>
+          <Button
+            buttonStyle={styles.btnYouWant}
+            type="outline"
+          >
+            <Icon
+              color={COLOR_BLUE_1}
+              name="radio-button-checked"
+              size={20}
+            />
+            <Text style={styles.txtCheck}>{t('button.postPublic')}</Text>
+          </Button>
+        </View>
+        <View style={styles.line} />
+        <View style={styles.boxDotLine}>
+          <View style={styles.dot([0, 1, 2].includes(tab))} />
+          <View style={styles.line1([1, 2].includes(tab))} />
+          <View style={styles.dot([1, 2].includes(tab))} />
+          <View style={styles.line1(tab === 2)} />
+          <View style={styles.dot(tab === 2)} />
+        </View>
+        <View style={styles.boxTab}>
+          <Text
+            style={styles.labelTab(tab === 0)}
+            onPress={() => handleTab(0)}
+          >
+            Thông tin cơ bản
+          </Text>
+          <Text
+            style={styles.labelTab(tab === 1)}
+            onPress={() => handleTab(1)}
+          >
+            Thông tin nhà đất
+          </Text>
+          <Text
+            style={styles.labelTab(tab === 2)}
+            onPress={() => handleTab(2)}
+          >
+            Chi tiết bài đăng
+          </Text>
+        </View>
+        {renderTab()}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default CreatePostScreen;
--- src/screens/createPost/CreatePostScreen/styles.js
+++ src/screens/createPost/CreatePostScreen/styles.js
@@ -1,10 +1,145 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+
+import {
+  COLOR_BLACK_1,
+  COLOR_BLUE_1,
+  COLOR_BLUE_2,
+  COLOR_GRAY_5,
+  COLOR_GRAY_7,
+  COLOR_ORANGE_6,
+  COLOR_WHITE,
+} from '../../../constants';
+
+const { width } = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
-  container: {
+  boxCheck: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  boxDotLine: {
     alignItems: 'center',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    marginTop: 7,
+  },
+  boxSelectAddress: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+  },
+  boxTab: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 7,
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+  boxType: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 16,
+    paddingHorizontal: 10,
+  },
+  btnYouWant: {
+    borderColor: COLOR_BLUE_1,
+    borderRadius: 5,
+    height: 40,
+    width: width * 0.45,
+  },
+  buySell: {
+    flex: 0.49,
+    marginBottom: 6,
+    marginHorizontal: 4,
+  },
+  checked: {
+    position: 'absolute',
+    right: 4,
+    top: 4,
+  },
+  container: {
+    backgroundColor: COLOR_WHITE,
     flex: 1,
-    justifyContent: 'center',
+  },
+  createPostNews: {
+    fontWeight: 500,
+  },
+  dot: select => ({
+    backgroundColor: select ? COLOR_ORANGE_6 : COLOR_GRAY_5,
+    borderRadius: 7,
+    height: 10,
+    width: 10,
+  }),
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+  inputLabel: {
+    color: COLOR_GRAY_7,
+    marginTop: 16,
+  },
+  isBuy: selected => ({
+    borderColor: selected ? COLOR_BLUE_2 : COLOR_GRAY_5,
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 0,
+    height: 40,
+  }),
+  labelTab: select => ({
+    color: COLOR_BLACK_1,
+    fontSize: 15,
+    fontWeight: select ? 'bold' : 400,
+  }),
+  line: {
+    backgroundColor: COLOR_GRAY_5,
+    height: 1,
+    marginVertical: 10,
+    width: '100%',
+  },
+  line1: select => ({
+    backgroundColor: select ? COLOR_ORANGE_6 : COLOR_GRAY_5,
+    height: 2,
+    marginHorizontal: 5,
+    width: '30%',
+  }),
+  scroll: {
+    paddingBottom: 50,
+  },
+  select: {
+    height: 40,
+    // padding: 10,
+  },
+  select1: {
+    height: 40,
+    width: width * 0.45,
+    // padding: 10,
+  },
+  txtCheck: {
+    color: COLOR_BLUE_1,
+    fontSize: 14,
+    lineHeight: 22,
+    marginLeft: 10,
+  },
+  txtType: select => ({
+    color: select ? COLOR_BLUE_1 : COLOR_GRAY_7,
+    fontWeight: 'bold',
+  }),
+  youWant: {
+    fontWeight: 500,
+    paddingHorizontal: 10,
+  },
+  youWantCenter: {
+    alignItems: 'center',
+    fontWeight: 500,
+    marginTop: 40,
+    paddingHorizontal: 10,
+    textAlign: 'center',
   },
 });
 
