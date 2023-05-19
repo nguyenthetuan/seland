cabe1ce857347a077441c0b057921bcf95ba3797
long.hoang <long.hoang@vnextglobal.com>
2023-05-19 09:32:11 +0700

    UI popup confirm

---
+++ src/screens/createPost/components/PopupConfirm/index.js
@@ -0,0 +1,72 @@
+import { useNavigation } from '@react-navigation/native';
+import { Button } from '@rneui/base';
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { Modal, View } from 'react-native';
+
+import { Text } from '../../../../components';
+import {
+  COLOR_BLUE_1,
+  COLOR_GRAY_4,
+  COLOR_GRAY_7,
+} from '../../../../constants';
+import ItemConfirm from '../ItemConfirm';
+import styles from './styles';
+
+const PopupConfirmPost = forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false);
+  const { navigate } = useNavigation();
+
+  const openPopup = () => setVisible(true);
+
+  const closePopup = () => setVisible(false);
+
+  useImperativeHandle(ref, () => ({ openPopup }));
+
+  return (
+    <Modal
+      visible={visible}
+      transparent
+    >
+      <View style={styles.container}>
+        <View style={styles.boxPopup}>
+          <Text style={styles.posting}>Tin đăng đã được ghi nhận!</Text>
+          <Text style={styles.youPost}>
+            Tin của bạn sẽ được kiểm duyệt trong 8h làm việc.
+          </Text>
+          <View style={styles.boxCodePost}>
+            <Text style={{ fontWeight: '500' }}>Mã tin đăng</Text>
+            <View style={styles.boxCode}>
+              <Text style={styles.code}>346582154</Text>
+            </View>
+          </View>
+          <ItemConfirm
+            label="Thanh toán"
+            value="Vip Bạc"
+          />
+          <ItemConfirm
+            label="Đơn giá/ ngày"
+            value="50,000 VNĐ"
+          />
+          <View style={styles.boxButton}>
+            <Button
+              buttonStyle={[styles.btnPopup, { borderColor: COLOR_GRAY_4 }]}
+              titleStyle={{ color: COLOR_GRAY_7 }}
+              title="Quản lý đăng tin"
+              type="outline"
+              onPress={closePopup}
+            />
+            <Button
+              buttonStyle={[styles.btnPopup, { backgroundColor: COLOR_BLUE_1 }]}
+              title="Đăng tin khác"
+              onPress={closePopup}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+});
+
+PopupConfirmPost.displayName = 'PopupConfirmPost';
+
+export default PopupConfirmPost;
---
+++ src/screens/createPost/components/PopupConfirm/styles.js
@@ -0,0 +1,69 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+import {
+  COLOR_BLACK_2,
+  COLOR_GREEN_1,
+  COLOR_ORANGE_6,
+  COLOR_WHITE,
+} from '../../../../constants';
+
+const { width } = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  boxButton: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  boxCode: {
+    borderColor: COLOR_ORANGE_6,
+    borderRadius: 4,
+    borderStyle: 'dotted',
+    borderWidth: 1,
+    marginLeft: 30,
+    padding: 5,
+    paddingHorizontal: 16,
+  },
+  boxCodePost: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    marginBottom: 26,
+    marginTop: 36,
+  },
+  boxPopup: {
+    backgroundColor: COLOR_WHITE,
+    borderRadius: 8,
+    height: width * 0.9,
+    padding: 20,
+    width: width * 0.96,
+  },
+  btnPopup: {
+    borderRadius: 5,
+    width: width * 0.4,
+  },
+  code: {
+    color: COLOR_ORANGE_6,
+  },
+  container: {
+    alignItems: 'center',
+    backgroundColor: COLOR_BLACK_2,
+    flexDirection: 'row',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  posting: {
+    color: COLOR_GREEN_1,
+    fontSize: 24,
+    lineHeight: 32,
+    marginBottom: 10,
+    marginTop: 16,
+    textAlign: 'center',
+  },
+  youPost: {
+    fontSize: 14,
+    lineHeight: 22,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
