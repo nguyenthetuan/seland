c53341acff64f9957fd5b30c233d4e68a50f9d8e
Phu Phan Van <phu.phan@vnextglobal.com>
2023-06-07 17:26:38 +0000

    Feature/filter post

--- src/screens/home/ListPostsScreen/index.js
+++ src/screens/home/ListPostsScreen/index.js
@@ -12,7 +12,6 @@ import {
   selectRealEstates,
 } from '../../../features/realEstates';
 import { dispatchThunk } from '../../../utils';
-import Filter from '../components/FilterModal';
 import HeaderFilterPosts from '../components/HeaderFilterPosts';
 import HeaderListPosts from '../components/HeaderListPosts';
 import ItemRealEstates from '../components/ItemRealEstates';
@@ -32,6 +31,7 @@ const ListPostsScreen = () => {
   const dispatch = useDispatch();
   const { data: listPosts, loading: loadingListPost } =
     useSelector(selectRealEstates);
+
   const onSelect = value => {
     console.log('🚀 ~ file: index.js:51 ~ onSelect ~ value:', value);
   };
@@ -42,9 +42,14 @@ const ListPostsScreen = () => {
 
   const convertDataFilter = (data) => {
     const res = {};
-    res.price_range_id = `${Number(data?.priceRange[0])/(10**9)}-${Number(data?.priceRange[1])/(10**9)}`;
-    res.area_range_id = `${Number(data?.acreage[0])}-${Number(data?.acreage[1])}`;
     res.sort_by = 'asc';
+
+    if (data?.priceRange?.length > 0) {
+      res.price_range_id = `${Number(data?.priceRange[0])/(10**9)}-${Number(data?.priceRange[1])/(10**9)}`;
+    }
+    if (data?.acreage?.length > 0) {
+      res.area_range_id = `${Number(data?.acreage[0])}-${Number(data?.acreage[1])}`;
+    }
     if (data?.typeHousing?.length > 0) {
       res.real_estate_type_id = data?.typeHousing?.join(',');
     }
@@ -62,6 +67,15 @@ const ListPostsScreen = () => {
     }
     if (data?.location > 0) {
       res.location_id = data?.location.join(',');
+    }
+    if (data?.province_id) {
+      res.province_id = data?.province_id;
+    }
+    if (data?.ward_id) {
+      res.ward_id = data?.ward_id;
+    }
+    if (data?.district_id) {
+      res.district_id = data?.district_id;
     }
     return res;
   }
