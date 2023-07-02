4e0fc928623128d2a8a3f305209082bce929ff88
longdevit <longcomputer9x@gmail.com>
2023-07-02 21:35:48 +0700

    fix bug and refactor code js to ts

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -1,15 +1,16 @@
-import { Text as RNEText } from '@rneui/themed';
+import { Text as RNEText, TextProps } from '@rneui/themed';
+
 import React, { ReactNode } from 'react';
 
 import styles from './styles';
 
-interface TextProps {
+interface TextCustomProps extends TextProps {
   children: ReactNode;
   style?: {} | [];
   onPress?: () => void;
 }
 
-const Text = ({ children, style = {}, ...props }: TextProps) => (
+const Text = ({ children, style = {}, ...props }: TextCustomProps) => (
   <RNEText
     style={
       Array.isArray(style) ? [styles.text, ...style] : [styles.text, style]
